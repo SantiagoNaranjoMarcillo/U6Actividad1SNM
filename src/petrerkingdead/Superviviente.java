0000f9a39d19810ceceebcdc2305405da8d6b0fa
zacht3000 <zacht3000@DESKTOP-ULVUR77>
2019-12-19 11:17:02 +0100

    Creado y probado metodo de mueveSuperviviente, este metodo sirve para que el superviviente pueda moverse a determinadas casillas anteponiendose alos eventos

--- src/petrerkingdead/Superviviente.java
+++ src/petrerkingdead/Superviviente.java
@@ -5,11 +5,31 @@ package petrerkingdead;
  * @author Santiago Naranjo Marcillo
  */
 public class Superviviente {
-    
+
     public static final String SUPERVIVIENTE = "S";
-    
+
     public static int[] posicionInicialSuperviviente() {
         int[] posicion = {2, 1};
         return posicion;
+    }
+
+    public static void mueveSuperviviente(int[] posicion, char respuesta, int[][] escenario) {
+        if (respuesta == 'w' //Arriba
+                && Casillas.noHayLimite(escenario, posicion[0] - 1, posicion[1])
+                && Casillas.vaciaCasilla(escenario, posicion[0], posicion[1])) {
+            posicion[0] -= 1; 
+        } else if (respuesta == 's'//Abajo
+                && Casillas.noHayLimite(escenario, posicion[0] + 1, posicion[1])
+                && Casillas.vaciaCasilla(escenario, posicion[0], posicion[1])) {
+            posicion[0] += 1;
+        } else if (respuesta == 'a'//Derecha
+                && Casillas.noHayLimite(escenario, posicion[0], posicion[1] - 1)
+                && Casillas.vaciaCasilla(escenario, posicion[0], posicion[1])) {
+            posicion[1] -= 1;
+        } else if (respuesta == 'd'//Izquierda
+                && Casillas.noHayLimite(escenario, posicion[0], posicion[1] + 1)
+                && Casillas.vaciaCasilla(escenario, posicion[0], posicion[1])) {
+            posicion[1] += 1;
+        }
     }
 }
