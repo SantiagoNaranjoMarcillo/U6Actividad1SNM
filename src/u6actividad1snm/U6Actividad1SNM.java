d5c63241c2e444f654e9cd26218415543391bd66
zacht3000 <zacht3000@DESKTOP-ULVUR77>
2019-12-19 01:06:25 +0100

    Creado repositorio del juego PetrerKing Dead.

---
+++ src/u6actividad1snm/U6Actividad1SNM.java
@@ -0,0 +1,13 @@
+package petrerkingdead;
+
+/**
+ *
+ * @author Santiago Naranjo Marcillo
+ */
+public class U6Actividad1SNM {
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
