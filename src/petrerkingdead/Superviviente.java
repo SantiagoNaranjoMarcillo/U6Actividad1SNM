df12de5e3f0683c0d5a50962dfd57ee69042ae04
zacht3000 <zacht3000@DESKTOP-ULVUR77>
2019-12-19 01:45:19 +0100

    Creado metodo posicionInicialSuperviviente para guardar las posiciones del supervieinte en un array,

--- src/petrerkingdead/Superviviente.java
+++ src/petrerkingdead/Superviviente.java
@@ -8,4 +8,8 @@ public class Superviviente {
     
     public static final String SUPERVIVIENTE = "S";
     
+    public static int[] posicionInicialSuperviviente() {
+        int[] posicion = {2, 1};
+        return posicion;
+    }
 }
