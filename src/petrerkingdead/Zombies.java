d524fd22b6265ecfbd49590364d24fcfec8b2728
zacht3000 <zacht3000@DESKTOP-ULVUR77>
2019-12-20 03:26:31 +0100

    Creado metodo de mover aleatoramente a los zombies junto que se detengan cuando los zombies comen a un superviviente

--- src/petrerkingdead/Zombies.java
+++ src/petrerkingdead/Zombies.java
@@ -14,4 +14,60 @@ public class Zombies {
         int[][] posicion = {{3, 1}, {3, 4}, {5, 6}};
         return posicion;
     }
+
+    public static void mueveATodosLosZombies(int[][] posicion, int[][] escenario, int[] posicionSuperviviente) {
+        Random rnd = new Random();
+
+        int zombie = rnd.nextInt(2);;
+        boolean correctoZombie = false;
+        int movimientoRandom = rnd.nextInt(3);;
+        int cont = 0;
+        do {
+            switch (movimientoRandom) {
+                            case 0://arriba
+                                if (Casillas.noHayLimite(escenario, posicion[zombie][0] - 1, posicion[zombie][1])
+                                        && posicionSuperviviente[0] != posicion[zombie][0]) {
+                                    posicion[zombie][0] -= 1;
+                                    correctoZombie = true;
+                                }
+
+                                break;
+                            case 1://abajo
+                                if (Casillas.noHayLimite(escenario, posicion[zombie][0] + 1, posicion[zombie][1])
+                                        && posicionSuperviviente[0] != posicion[zombie][0]) {
+                                    posicion[zombie][0] += 1;
+                                    correctoZombie = true;
+                                }
+
+                                break;
+                            case 2://derecha
+                                if (Casillas.noHayLimite(escenario, posicion[zombie][0], posicion[zombie][1] + 1)
+                                        && posicionSuperviviente[1] != posicion[zombie][1]) {
+                                    posicion[zombie][1] += 1;
+                                    correctoZombie = true;
+                                }
+
+                                break;
+                            case 3://izquierda
+                                if (Casillas.noHayLimite(escenario, posicion[zombie][0], posicion[zombie][1] - 1)
+                                        && posicionSuperviviente[1] != posicion[zombie][1]) {
+                                    posicion[zombie][1] -= 1;
+                                    correctoZombie = true;
+                                }
+
+                                break;
+                        }
+                    if (cont++ > 3)
+                        movimientoRandom += 1;
+                    if (cont == 3)
+                        movimientoRandom = 0;
+                    if (cont < 3)
+                        zombie = rnd.nextInt(2);
+                    if(cont == 8)
+                        correctoZombie = true;
+
+                } while (!correctoZombie);
+
+            }
+
 }
