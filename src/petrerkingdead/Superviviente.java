018104fa8cc35a04691c23c37e57e8e15353f883
zacht3000 <zacht3000@DESKTOP-ULVUR77>
2020-01-13 23:10:18 +0100

    Corregido el las respuesta para que se puede añadir minusculas y mayusculas, y se quito el vacia casillas por que no se podía acceder.

--- src/petrerkingdead/Superviviente.java
+++ src/petrerkingdead/Superviviente.java
@@ -9,26 +9,22 @@ public class Superviviente {
     public static final String SUPERVIVIENTE = "S";
 
     public static int[] posicionInicialSuperviviente() {
-        int[] posicion = {2, 1};
+        int[] posicion = {1, 8};
         return posicion;
     }
 
-    public static void mueveSuperviviente(int[] posicion, char respuesta, int[][] escenario) {
-        if (respuesta == 'w' //Arriba
-                && Casillas.noHayLimite(escenario, posicion[0] - 1, posicion[1])
-                && Casillas.vaciaCasilla(escenario, posicion[0], posicion[1])) {
+    public static void mueveSuperviviente(int[] posicion, String respuesta, int[][] escenario) {
+        if (respuesta.equalsIgnoreCase("W") //Arriba
+                && Casillas.noHayLimite(escenario, posicion[0] - 1, posicion[1])) {
             posicion[0] -= 1; 
-        } else if (respuesta == 's'//Abajo
-                && Casillas.noHayLimite(escenario, posicion[0] + 1, posicion[1])
-                && Casillas.vaciaCasilla(escenario, posicion[0], posicion[1])) {
+        } else if (respuesta.equalsIgnoreCase("S")//Abajo
+                && Casillas.noHayLimite(escenario, posicion[0] + 1, posicion[1])) {
             posicion[0] += 1;
-        } else if (respuesta == 'a'//Derecha
-                && Casillas.noHayLimite(escenario, posicion[0], posicion[1] - 1)
-                && Casillas.vaciaCasilla(escenario, posicion[0], posicion[1])) {
+        } else if (respuesta.equalsIgnoreCase("A")//Derecha
+                && Casillas.noHayLimite(escenario, posicion[0], posicion[1] - 1)) {
             posicion[1] -= 1;
-        } else if (respuesta == 'd'//Izquierda
-                && Casillas.noHayLimite(escenario, posicion[0], posicion[1] + 1)
-                && Casillas.vaciaCasilla(escenario, posicion[0], posicion[1])) {
+        } else if (respuesta.equalsIgnoreCase("D")//Izquierda
+                && Casillas.noHayLimite(escenario, posicion[0], posicion[1] + 1)) {
             posicion[1] += 1;
         }
     }
